ced9053e85f4f5eb46784397f5ad99df0eb6560f
ArsenUtepbergenov <arsen.utepbergenov@yandex.ru>
2022-11-22 23:10:01 +0100

    update: dev/minor

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,25 +3,25 @@ import { Ships } from '@/models/enums'
 export const URL = 'http://localhost:9000'
 
 export const TypeShips = [
-  { amount: 1, type: Ships.TorpedoBoat },
-  { amount: 1, type: Ships.Destroyer },
-  // { amount: 1, type: Ships.Cruiser },
-  // { amount: 1, type: Ships.Battleship },
-]
+  { amount: 4, type: Ships.TorpedoBoat },
+  { amount: 3, type: Ships.Destroyer },
+  { amount: 2, type: Ships.Cruiser },
+  { amount: 1, type: Ships.Battleship },
+] as const
 
 export abstract class Config {
-  static size = 360
-  static cells = 10
-  static cellSize = Math.floor(Config.size / Config.cells)
-  static halfCellSize = Config.cellSize / 2
-  static gridColor = '#a88213'
-  static shipColor = '#e4e4e4'
-  static successShotColor = '#8bc34a'
-  static missedShotColor = '#2196f3'
-  static failShotColor = '#ff4336'
-  static shipSize = Config.cellSize
-  static shipsSpotSize = Config.shipSize * 7
-  static gridPositionsSize = Config.size / Config.cellSize
-  static numberShips = TypeShips.reduce((a, prev) => a + prev.amount, 0)
-  static numberShipCells = TypeShips.reduce((a, prev) => a + prev.amount * prev.type, 0)
+  static readonly size = 360
+  static readonly cells = 10
+  static readonly cellSize = Math.floor(Config.size / Config.cells)
+  static readonly halfCellSize = Config.cellSize / 2
+  static readonly gridColor = '#a88213'
+  static readonly shipColor = '#e4e4e4'
+  static readonly successShotColor = '#8bc34a'
+  static readonly missedShotColor = '#2196f3'
+  static readonly failShotColor = '#ff4336'
+  static readonly shipSize = Config.cellSize
+  static readonly shipsSpotSize = Config.shipSize * 7
+  static readonly gridPositionsSize = Config.size / Config.cellSize
+  static readonly numberShips = TypeShips.reduce((a, prev) => a + prev.amount, 0)
+  static readonly numberShipCells = TypeShips.reduce((a, prev) => a + prev.amount * prev.type, 0)
 }
